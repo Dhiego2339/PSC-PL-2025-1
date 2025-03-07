1fdae81cd3fb5b5c60062c9613143743eb2ab147
Dhiego2339 <82426996@ulife.com.br>
2025-03-07 11:33:03 -0300

    Criação dos operadores na classe

--- Operadores.java
+++ Operadores.java
@@ -1,5 +1,18 @@
 public class Operadores {
+
     public static void main(String[] args) {
+//Operadores Aritméticos 
+int = 10;
+int = 5;
+System.out.println(a + b );
+System.out.println(a - b );
+System.out.println(a / b );
+System.out.println(a * b );
+System.out.println(a % b );
+
+
+
+
          System.out.println("Olá mundo");
     }
 }
